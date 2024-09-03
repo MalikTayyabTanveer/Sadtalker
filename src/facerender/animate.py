8938c73d4d71a8f3da8388ed3e37ea7ecc9bb112
Malik Tayyab Tanveer <138617303+MalikTayyabTanveer@users.noreply.github.com>
2024-09-03 18:05:20 +0500

    frame display

--- src/facerender/animate.py
+++ src/facerender/animate.py
@@ -182,76 +182,37 @@ class AnimateFromCoeff():
 
         predictions_video = make_animation(source_image, source_semantics, target_semantics,
                                         self.generator, self.kp_extractor, self.he_estimator, self.mapping, 
-                                        yaw_c_seq, pitch_c_seq, roll_c_seq, use_exp = True)
+                                        yaw_c_seq, pitch_c_seq, roll_c_seq, use_exp=True)
 
         predictions_video = predictions_video.reshape((-1,)+predictions_video.shape[2:])
         predictions_video = predictions_video[:frame_num]
 
-        video = []
         for idx in range(predictions_video.shape[0]):
             image = predictions_video[idx]
             image = np.transpose(image.data.cpu().numpy(), [1, 2, 0]).astype(np.float32)
-            video.append(image)
-        result = img_as_ubyte(video)
-
-        ### the generated video is 256x256, so we keep the aspect ratio, 
-        original_size = crop_info[0]
-        if original_size:
-            result = [ cv2.resize(result_i,(img_size, int(img_size * original_size[1]/original_size[0]) )) for result_i in result ]
-        
-        video_name = x['video_name']  + '.mp4'
-        path = os.path.join(video_save_dir, 'temp_'+video_name)
-        
-        imageio.mimsave(path, result,  fps=float(25))
-
-        av_path = os.path.join(video_save_dir, video_name)
-        return_path = av_path 
-        
-        audio_path =  x['audio_path'] 
-        audio_name = os.path.splitext(os.path.split(audio_path)[-1])[0]
-        new_audio_path = os.path.join(video_save_dir, audio_name+'.wav')
-        start_time = 0
-        # cog will not keep the .mp3 filename
-        sound = AudioSegment.from_file(audio_path)
-        frames = frame_num 
-        end_time = start_time + frames*1/25*1000
-        word1=sound.set_frame_rate(16000)
-        word = word1[start_time:end_time]
-        word.export(new_audio_path, format="wav")
-
-        save_video_with_watermark(path, new_audio_path, av_path, watermark= False)
-        print(f'The generated video is named {video_save_dir}/{video_name}') 
-
-        if 'full' in preprocess.lower():
-            # only add watermark to the full image.
-            video_name_full = x['video_name']  + '_full.mp4'
-            full_video_path = os.path.join(video_save_dir, video_name_full)
-            return_path = full_video_path
-            paste_pic(path, pic_path, crop_info, new_audio_path, full_video_path, extended_crop= True if 'ext' in preprocess.lower() else False)
-            print(f'The generated video is named {video_save_dir}/{video_name_full}') 
-        else:
-            full_video_path = av_path 
-
-        #### paste back then enhancers
-        if enhancer:
-            video_name_enhancer = x['video_name']  + '_enhanced.mp4'
-            enhanced_path = os.path.join(video_save_dir, 'temp_'+video_name_enhancer)
-            av_path_enhancer = os.path.join(video_save_dir, video_name_enhancer) 
-            return_path = av_path_enhancer
-
-            try:
-                enhanced_images_gen_with_len = enhancer_generator_with_len(full_video_path, method=enhancer, bg_upsampler=background_enhancer)
-                imageio.mimsave(enhanced_path, enhanced_images_gen_with_len, fps=float(25))
-            except:
-                enhanced_images_gen_with_len = enhancer_list(full_video_path, method=enhancer, bg_upsampler=background_enhancer)
-                imageio.mimsave(enhanced_path, enhanced_images_gen_with_len, fps=float(25))
             
-            save_video_with_watermark(enhanced_path, new_audio_path, av_path_enhancer, watermark= False)
-            print(f'The generated video is named {video_save_dir}/{video_name_enhancer}')
-            os.remove(enhanced_path)
+            # Resize image to original size if needed
+            if crop_info[0]:
+                original_size = crop_info[0]
+                image = cv2.resize(image, (img_size, int(img_size * original_size[1]/original_size[0])))
+            
+            image = img_as_ubyte(image)
+            
+            # Display the frame
+            cv2.imshow('Generated Frame', image)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
 
-        os.remove(path)
-        os.remove(new_audio_path)
+        # After the loop ends, make sure to close the window
+        cv2.destroyAllWindows()
 
-        return return_path
+        # The rest of your code to handle saving or further processing if needed
+        # For example, you could still save the video if desired, but without moving to result dir.
+        video_name = x['video_name']  + '.mp4'
+        path = os.path.join(video_save_dir, 'temp_' + video_name)
+        
+        # Save video if needed
+        imageio.mimsave(path, [img_as_ubyte(image) for image in predictions_video], fps=float(25))
+        return path
+
 
